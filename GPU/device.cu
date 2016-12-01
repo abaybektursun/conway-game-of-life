08d3567d2fb519d77732a54a449b558f6fccd600
Abay Bektursun <abaybektursun@localhost.localdomain>
2016-12-01 21:30:03 +0000

    Lastest changes

--- GPU/device.cu
+++ GPU/device.cu
@@ -1,27 +1,46 @@
 #include "utils.h"
+#include <stdio.h>
 
 
-// Mod. operation for preventing out of boundry index
-#define live_cells( x, y, xN, yN,world ) (    world[abs((x-1)%xN)][abs((y-1)%yN)] + world[x][abs((y-1)%yN)] + world[abs((x+1)%xN)][abs((y-1)%yN)]     \
-                                   +          world[abs((x-1)%xN)][y]        +                                world[abs((x+1)%xN)][y]                 \
-                                   +          world[abs((x-1)%xN)][abs((y+1)%yN)] + world[x][abs((y+1)%yN)] + world[abs((x+1)%xN)][abs((y+1)%yN)]     )
+#define live_cells( thread_1D_pos, x, y, world ) (    world[(thread_1D_pos-x-1)%(x*y)] + world[(thread_1D_pos-x)%(x*y)] + world[(thread_1D_pos-x+1)%(x*y)]     \
+                                   +                  world[(thread_1D_pos - 1)%(x*y)] +                                  world[(thread_1D_pos-x+1)%(x*y)]     \
+                                   +                  world[(thread_1D_pos+x-1)%(x*y)] + world[(thread_1D_pos+x)%(x*y)] + world[(thread_1D_pos+x+1)%(x*y)]     )
 
 __global__
-void cell_live(uchar1** world)
+void cell_live(const unsigned char* const in_world,
+                     unsigned char* const out_world,
+                             unsigned int numRows,
+                             unsigned int numCols)
 {
+    const int2 thread_2D_pos = make_int2( blockIdx.x * blockDim.x + threadIdx.x,
+                                          blockIdx.y * blockDim.y + threadIdx.y);
+    // Check for out of boundries
+    if (thread_2D_pos.x >= numCols || thread_2D_pos.y >= numRows)
+      return;
 
+    const unsigned int thread_1D_pos  = thread_2D_pos.y * numCols + thread_2D_pos.x;
+    unsigned int alive_neighbors = live_cells(thread_1D_pos, numCols, numRows, in_world);
+    alive_neighbors /= 255;
+    if (alive_neighbors == 3 || (alive_neighbors == 2 && in_world[thread_1D_pos]))
+        out_world[thread_1D_pos] = 255;
+    else
+        out_world[thread_1D_pos] = 0;
 }
 
-void game_of_life_cuda(const uchar1* const h_inWorld,
-                             uchar1* const d_inWorld,
-                             uchar1* const d_outWorld,
+void game_of_life_cuda(unsigned char* const d_inWorld,
+                       unsigned char* const d_outWorld,
                        const size_t numRows,
                        const size_t numCols)
 {
+    const dim3 blockSize ( 32,32,1 );
 
-}
+                              // Ceiling
+    const dim3 gridSize ( 1 + ((numCols - 1) / blockSize.x),   1 + ((numRows - 1) / blockSize.y)   ,1 );
 
-void memoryManagement(const size_t numRows, const size_t numCols)
-{
+    // Launch a kernel
+    cell_live <<<gridSize, blockSize>>>(d_inWorld, d_outWorld, numRows, numCols);
+
+    // Make sure I didn't did not mess this up like I did with my last relationship
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
 }
--- GPU/main.cc
+++ GPU/main.cc
@@ -2,40 +2,68 @@
 #include "utils.h"
 #include <string>
 #include <stdio.h>
+#include <pthread.h>
+#include <thread>
+#include <string>
 #include "routines.cc"
 
+#define ITERS 100
+
 // parallel.cu  ----------------------------------------------------//
-void game_of_life_cuda(const uchar1* const h_inWorld,               //
-                       uchar1* const d_inWorld,                     //
-                       uchar1* const d_outWorld,                    //
-                       const size_t numRows, const size_t numCols); //
-                                                                    //
-void memoryManagement(const size_t numRows, const size_t numCols);  //
+void game_of_life_cuda(unsigned char* const d_inWorld,
+                       unsigned char* const d_outWorld,
+                       const size_t numRows,
+                       const size_t numCols);
 // parallel.cu  ----------------------------------------------------//
 
 int main()
 {
-    uchar1 *h_inWorld,  *d_inWorld;
-    uchar1 *h_outWorld, *d_outWorld;
+    unsigned char *h_inWorld,  *d_inWorld;
+    unsigned char *h_outWorld, *d_outWorld;
+    unsigned char *d_outWorld_swap;
 
     std::string in_file_name = "imgs/0.png";
 
     //load the image and get the pointers
     preProcess(&h_inWorld, &h_outWorld,
                &d_inWorld, &d_outWorld,
-               in_file_name           );
+                      &d_outWorld_swap,
+                          in_file_name);
 
-    memoryManagement(numRows(), numCols());
+    memoryManagement(&h_inWorld, &d_inWorld, &d_outWorld, &d_outWorld_swap);
 
-    // CUDA
-    game_of_life_cuda(h_inWorld, d_inWorld, d_outWorld, numRows(), numCols());
-    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
     size_t numPixels = numRows()*numCols();
-    checkCudaErrors(cudaMemcpy(h_outWorld, d_outWorld__, sizeof(uchar1) * numPixels, cudaMemcpyDeviceToHost));
+    // CUDA
+    game_of_life_cuda(d_inWorld, d_outWorld, numRows(), numCols());
+    // Get yo ass back to host
+    checkCudaErrors(cudaMemcpy(h_outWorld, d_outWorld__, sizeof(unsigned char) * numPixels, cudaMemcpyDeviceToHost));
+    unsigned i;
+    for(i = 2; i < ITERS; i++){
+        std::thread writer([&](){
+            save8UC1Image(&h_outWorld, std::to_string(i) + ".png");
+        });
+        std::swap(d_inWorld, d_outWorld);
 
+        game_of_life_cuda(d_inWorld, d_outWorld, numRows(), numCols());
+        // Get yo ass back to host
+        checkCudaErrors(cudaMemcpy(h_outWorld, d_outWorld__, sizeof(unsigned char) * numPixels, cudaMemcpyDeviceToHost));
+
+        writer.join();
+    }
+    cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+    save8UC1Image(&h_outWorld, std::to_string(i) + ".png");
+
+
+    /*
+    size_t numPixels = numRows()*numCols();
+    checkCudaErrors(cudaMemcpy(h_outWorld, d_outWorld__, sizeof(unsigned char) * numPixels, cudaMemcpyDeviceToHost));
     postProcess("imgs/2.png",h_outWorld);
+    checkCudaErrors(cudaFree(d_inWorld));
+    checkCudaErrors(cudaFree(d_outWorld));
+    */
 
+    free(h_outWorld);
     cleanUp();
     return 0;
 }
